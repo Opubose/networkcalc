05f13c992db27c855a577dbd4608500320eb73b0
Opubose <agastyabose@hotmail.com>
2025-03-08 00:30:12 -0600

    Placeholders

---
+++ src/MathClient.java
@@ -0,0 +1,5 @@
+public class MathClient {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
---
+++ src/MathServer.java
@@ -0,0 +1,5 @@
+public class MathServer {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
